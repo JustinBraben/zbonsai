0223598bc01126eaf55c8b4e0796ae4089163856
Justin Braben <jbraben@dmsiworks.com>
2024-10-17 12:50:20 -0600

    Making some more widgets

---
+++ src/utils/Progressbar.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+const vaxis = @import("vaxis");
+const Window = vaxis.Window;
+
+const Progressbar = @This();
+
+/// character to use for the Progressbar
+complete_character: vaxis.Cell.Character = .{ .grapheme = "█", .width = 1 },
+
+/// character to use for the Progressbar
+incomplete_character: vaxis.Cell.Character = .{ .grapheme = "▒", .width = 1 },
+
+/// style to draw the bar character with
+style: vaxis.Style = .{},
+
+width: usize = 10,
+
+height: usize = 1,
+
+/// Current progress of Progressbar.
+current_progress: usize = 0,
+/// Max progress of Progressbar
+max_progress: usize = 10,
+
+pub fn draw(self: Progressbar, win: vaxis.Window) void {
+    // Only height of 1 windows are supported for now
+    if (win.height != 1) return;
+
+    // const bar_height = std.math.clamp(self.height, 1, win.height);
+
+    var i: usize = 0;
+    while (i < self.max_progress and i < win.width) : (i +|= 1) {
+        if (i <= self.current_progress) {
+            win.writeCell(i, 0, .{ .char = self.complete_character, .style = self.style });
+        }
+        else {
+            win.writeCell(i, 0, .{ .char = self.incomplete_character, .style = self.style });
+        }
+    }
+}
+
+/// Adds completed progress to the bar
+pub fn addProgress(self: *Progressbar, progress: usize) void {
+    self.current_progress +|= progress;
+}
+
+const sample = 
+    \\█░░░░░░░░░
+    ;
---
+++ src/utils/Spinner.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+const vaxis = @import("vaxis");
+const Window = vaxis.Window;
+const Cell = vaxis.Cell;
+
+const Spinner = @This();
+
+/// Windows doesn't have access to these by default, although 
+/// can be supported with unicode
+const posix_characters = &[_][]const u8{
+    "⠋",
+    "⠙",
+    "⠹",
+    "⠸",
+    "⠼",
+    "⠴",
+    "⠦",
+    "⠧",
+    "⠇",
+    "⠏",
+};
+
+const windows_characters = &[_][]const u8{
+    "+",
+    "-",
+    "x",
+    "|",
+};
+
+/// character to use for the Progressbar
+character: vaxis.Cell.Character = .{ .grapheme = "+", .width = 1 },
+
+/// style to draw the bar character with
+style: vaxis.Style = .{},
+
+width: usize = 1,
+
+height: usize = 1,
+
+index: usize = 0,
+
+pub fn draw(self: Spinner, win: vaxis.Window) void {
+    // Only height of 1 windows are supported for now
+    // if (win.height != 1 or win.width != 0) return;
+
+    win.writeCell(0, 0, 
+    .{ 
+        .char = self.character, 
+        .style = self.style 
+    });
+}
+
+/// Adds completed progress to the bar
+pub fn update(self: *Spinner) void {
+    self.index +%= 1;
+    if (self.index >= posix_characters.len) self.index = 0;
+    self.character = .{ .grapheme = posix_characters[self.index], .width = 1 };
+}
+
+const sample = 
+    \\⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏
+    ;
